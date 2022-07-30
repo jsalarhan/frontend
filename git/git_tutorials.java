45e08e25f37372904790755c5b9ef1e36275a3b7
jsalarhan <jsalarhan@gmail.com>
2022-07-30 16:25:31 +0300

    first commit

---
+++ git/git_tutorials.java
@@ -0,0 +1,9 @@
+public git_tutorials{
+    git :işletim sistemi olarak dusunebiliriz
+
+        git neden önemlidir
+        
+        GitHub:bilgisayarımızın harddiski olarak dusunebiliriz
+
+
+        }
